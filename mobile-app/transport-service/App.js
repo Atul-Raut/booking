8d9f83afb0604469ca75e1814dc2de98f7a03297
Vira <raviaglave99@gmail.com>
2021-08-10 21:22:46 +0100

    Get Started Added

--- mobile-app/transport-service/App.js
+++ mobile-app/transport-service/App.js
@@ -4,9 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer } from '@react-navigation/native';
 import { createStackNavigator } from 'react-navigation-stack';
 
-import HomeScreen from './components/HomeScreen';
-import AboutScreen from './components/AboutScreen';
-import ChangePassword from './components/common/ChangePassword';
+import HomeScreen from "./components/HomeScreen";
+import AboutScreen from "./components/AboutScreen";
+import ChangePassword from "./components/common/ChangePassword";
+import GetStarted from "./components/GetStarted";
 
 export default class App extends React.Component {
   render() {
@@ -14,6 +15,13 @@ export default class App extends React.Component {
   }
 }
 const AppNavigator = createStackNavigator({
+  Start: {
+    screen: GetStarted,
+    navigationOptions: {
+      title: "",
+      headerStyle: { backgroundColor: "#009387" },
+    },
+  },
   Home: {
     screen: HomeScreen
   },
