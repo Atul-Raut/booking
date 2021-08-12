3bab309ce871d7bdb71e2a8df32dc1d486084b34
rautatul <rautatul@localhost>
2021-08-12 16:22:55 +0900

    Header changes

--- mobile-app/transport-service/components/common/BaseNavigation.js
+++ mobile-app/transport-service/components/common/BaseNavigation.js
@@ -7,21 +7,38 @@ import HomeScreen from "../HomeScreen";
 import AboutScreen from "../AboutScreen";
 import ChangePassword from "../common/ChangePassword";
 import SignUpScreen from "../SignUpScreen";
+import Header from "../common/Header"
+
 
 const RootStack = createStackNavigator();
 
-const BaseNavigation = ({ navigation }) => (
-  <RootStack.Navigator
-    screenOptions={{
-      headerShown: false,
-    }}
-  >
-    <RootStack.Screen name="GetStarted" component={GetStarted} />
-    <RootStack.Screen name="SignUpScreen" component={SignUpScreen} />
-    <RootStack.Screen name="HomeScreen" component={HomeScreen} />
-    <RootStack.Screen name="AboutScreen" component={AboutScreen} />
-    <RootStack.Screen name="ChangePassword" component={ChangePassword} />
-  </RootStack.Navigator>
-);
+const BaseNavigation = function StackScreen() {
+  return (
+    <RootStack.Navigator screenOptions={{headerTitleAlign: 'center', headerTitleStyle:{width:300, textAlign:"center"}}} navi>
+     <RootStack.Screen  name="GetStarted" component={GetStarted}
+      options={({ navigation }) => ({ headerShown:false})}/> 
+  
+      <RootStack.Screen name="SignUpScreen" component={SignUpScreen} />
+      <RootStack.Screen  name="HomeScreen" component={HomeScreen} 
+        options={({ navigation }) => ({
+          header: props => <Header navigation={navigation}/>,
+          headerLeft:null,
+          headerTitleStyle: { alignSelf: 'center' }
+        })}/>
+     <RootStack.Screen name="AboutScreen" component={AboutScreen}
+      options={({ navigation }) => ({
+        header: props => <Header navigation={navigation}/>,
+        headerLeft:null,
+        headerTitleStyle: { alignSelf: 'center' }
+      })}/>
+     <RootStack.Screen name="ChangePassword" component={ChangePassword} 
+       options={({ navigation }) => ({
+        header: props => <Header navigation={navigation}/>,
+        headerLeft:null,
+        headerTitleStyle: { alignSelf: 'center' }
+      })}/>
+    </RootStack.Navigator>
+  );
+};
 
 export default BaseNavigation;
