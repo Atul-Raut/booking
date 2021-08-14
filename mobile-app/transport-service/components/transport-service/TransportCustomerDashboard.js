9ea3477c08b73a3ef95d2d496b59b3f4e25be764
rautatul <rautatul44@gmail.com>
2021-08-14 21:34:24 +0900

    Dashboard screens

---
+++ mobile-app/transport-service/components/transport-service/TransportCustomerDashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View } from "react-native";
+import AppBaseComponent from "../common/AppBaseComponent";
+
+
+export default class TransportCustomerDashbord extends AppBaseComponent {
+  constructor(props){
+      super(props);
+}
+render() {
+  return (
+    <View>
+        Transport  Customer  Dashboard
+    </View>
+  );
+}
+}
---
+++ mobile-app/transport-service/components/transport-service/TransportServiceProviderDashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View } from "react-native";
+import AppBaseComponent from "../common/AppBaseComponent";
+
+
+export default class TransportServiceProviderDashbord extends AppBaseComponent {
+  constructor(props){
+      super(props);
+}
+render() {
+  return (
+    <View>
+        Transport S ervice   Provider   Dashboard
+    </View>
+  );
+}
+}
