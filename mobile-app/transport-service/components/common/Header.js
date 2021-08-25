cba42992579e0a24a996b755a95501eabee328cf
Vira <raviaglave99@gmail.com>
2021-08-25 11:07:04 +0100

    Header style changes

--- mobile-app/transport-service/components/common/Header.js
+++ mobile-app/transport-service/components/common/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { getSelectedServiceName } from "./AppBaseComponent";
-import {globalStyles} from '../common/GlobalStyles';
+import { globalStyles } from "../common/GlobalStyles";
 
 export default function Header({ navigation, name }) {
   const openMenu = () => {
@@ -12,16 +12,25 @@ export default function Header({ navigation, name }) {
     <View style={globalStyles.header}>
       <MaterialIcons
         name="menu"
-        color='white'
-        size={20}
+        color="white"
+        size={30}
         onPress={openMenu}
-        style={[globalStyles.icon,{marginTop:20}]}
+        style={[globalStyles.icon]}
       />
       <View>
-        <Text style={[globalStyles.headerText,{color:'white',marginTop:20}]}>{name}</Text>
+        <Text style={[globalStyles.headerText, { color: "white" }]}>
+          {name}
+        </Text>
       </View>
-       <View style={globalStyles.serviceNameText}>
-        <Text style={[globalStyles.serviceNameText2,{color:'white', fontWeight:'bold',marginTop:50}]}>{getSelectedServiceName()}</Text>
+      <View style={globalStyles.serviceNameText}>
+        <Text
+          style={[
+            globalStyles.serviceNameText2,
+            { color: "white", fontWeight: "bold" },
+          ]}
+        >
+          {getSelectedServiceName()}
+        </Text>
       </View>
     </View>
   );
